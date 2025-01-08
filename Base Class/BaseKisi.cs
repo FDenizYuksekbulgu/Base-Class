659e7ccedc3a541d826629f26cc1cb713271013b
Deniz Yuksekbukgu <fdenizyuksekbulgu@gmail.com>
2025-01-08 14:30:50 +0300

    Add project files.

---
+++ Base Class/BaseKisi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Base_Class
+{
+    public class BaseKisi // Burada tanımlanan özellikler ortak kullanılacak olanlardır.
+    {
+        public string Ad {  get; set; }
+        public string Soyad  { get; set; }
+        public void YazdirAdSoyad ()
+        {
+            Console.WriteLine($"Adı: {Ad}, Soyadı: {Soyad}");
+        }
+     }
+    public class Ogrenci : BaseKisi //Ortak özelliklerin dışında kalan özellikler burada tanımlanır.
+    {
+        public int OgrenciNo { get; set; }
+        public void YazdirOgrenci()
+        {
+            Console.WriteLine($"Adı: {Ad}, Soyadı: {Soyad}, Öğrenci Numaranız: {OgrenciNo}");
+        }
+    }
+    public class Ogretmen : BaseKisi //Ortak özelliklerin dışında kalan özellikler burada tanımlanır.
+    {
+        public decimal Maas { get; set; }
+        public void YazdirOgretmen()
+        {
+            Console.WriteLine($"Adı: {Ad}, Soyadı: {Soyad}, Maaşınız: {Maas}");
+        }
+    }
+}
---
+++ Base Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using Base_Class;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Öğrenci nesnesinin tüm özelliklerini yazdıralım.
+
+        Ogrenci Ogrenci1 = new Ogrenci();
+        Ogrenci1.Ad = "Jack";
+        Ogrenci1.Soyad = "London";
+        Ogrenci1.OgrenciNo = 989898;
+
+        //Öğrenci bilgilerinin tamamını bir bütün halde yazdıralım.
+        Ogrenci1.YazdirOgrenci();
+
+
+
+        //Öğretmen nesnesinin tüm özelliklerini yazdıralım.
+        Ogretmen Ogretmen1 = new Ogretmen();
+        Ogretmen1.Ad = "Cane";
+        Ogretmen1.Soyad = "Corso";
+        Ogretmen1.Maas = 70000M;
+
+        //Öğretmen bilgilerinin tamamını bir bütün halde yazdıralım.
+       Ogretmen1.YazdirOgretmen();
+    }
+}
